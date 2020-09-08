60ac9708b1c682fdad0c57aa665105c1ee960148
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-09-08 15:08:40 -0500

    added schema and seed for restaurants exercise

---
+++ seed.sql
@@ -0,0 +1,3 @@
+/* This is the seed, it contains the data*/
+INSERT INTO restaurants (name,distance, stars, category, favorite_dish,takeout,last_visit)
+    VALUES ('MCDS','5','3','fast_food','Cheeseburger','Yes','03-03-2020'),('Churchs','4','4','fast_food','Chicken','Yes','03-07-2020'),('Burger_King','8','2','fast_food','Whopper','Yes','03-16-2020');
